12deaa486b6022123977c254d3ea839ff8c903d8
tcweb-dev <tcwebdeveloper@yahoo.com>
2021-09-29 15:33:33 -0400

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,26 @@
+//
+//
+Developed by Terry Christman
+temchris@yahoo.com
+Free use, no permission needed.
+
+CSS @media max to min settings
+ all and (max-width: 990px) and (min-width: 771px)
+ all and (max-width: 770px) and (min-width: 691px) 
+ all and (max-width: 690px) and (min-width: 551px) 
+ all and (max-width: 550px) and (min-width: 436px)
+334
+
+PHOTO CREDITS:
+unsplash.com
+License:
+Unsplash photos are made to be used freely. Our license reflects that.
+All photos can be downloaded and used for free
+Commercial and non-commercial purposes
+No permission needed (though attribution is appreciated!)
+
+Beach:
+Adriel Kloppenburg, unsplash.com
+   Photo by <a href="https://unsplash.com/@adriel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Adriel Kloppenburg</a> on <a href="https://unsplash.com/s/photos/beach?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+    
---
+++ script.js
@@ -0,0 +1,33 @@
+//
+// Developed by Terry Christman
+// temchris@yahoo.com
+// Free use, no permission needed.
+//
+let logo = document.querySelectorAll('.logoClickListen');
+// For Development purposes only.
+// const width1 = document.querySelector('.screenW1');
+// const width2 = document.querySelector('.screenW2');
+// const width3 = document.querySelector('.screenW3');
+
+// logo[index] creates a new EventListner for each logo.NodeList item.
+logo.forEach((el, index) => {
+  logo[index] = el;
+
+  // Opens new window on logo click.
+  logo[index].addEventListener('click', (e) => {
+    e.preventDefault();
+    window.open('https://ccf-indy.com/', '', '');
+  });
+});
+
+// For Development purposes only.
+// function writeSize() {
+//   const clientWidth = document.documentElement.clientWidth;
+//   width1.textContent = `DOM Client Width: ${clientWidth} px`;
+//   width2.textContent = `Inner Width: ${window.innerWidth} px`;
+//   width3.textContent = ` Window OuterWidth ${window.outerWidth} px`;
+// }
+// window.addEventListener('resize', writeSize);
+//
+//
+// See also: window.onresize;
